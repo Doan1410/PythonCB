a889d277a2207c4b6862f0973456a05f4eaf998c
Doan1410 <ngothanhdoan2004@gmail.com>
2025-04-23 09:19:51 +0700

    Delete 2022603699_NgoThanhDoan directory

--- 2022603699_NgoThanhDoan/bai4.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-folder_name="BAI44"
-os.makedirs(folder_name,exist_ok=1)
-shutil.copy('DATA54.txt',folder_name)
-os.rename(os.path.join(folder_name,'DATA54.txt'),os.path.join(folder_name,'Data.dat'))
-os.remove(os.path.join(folder_name,'Data.dat'))
-os.rmdir(folder_name)
